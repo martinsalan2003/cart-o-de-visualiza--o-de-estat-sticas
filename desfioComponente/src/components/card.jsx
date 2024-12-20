d1abbdabec8f587ddd7e6e3fefa67d92f5d4eec6
AlanMartins00 <alanmartinsferreira59@gmail.com>
2024-12-19 23:40:21 -0300

    upload da primeira parte

---
+++ desfioComponente/src/components/card.jsx
@@ -0,0 +1,27 @@
+import '../styles/card.sass'
+
+function Card(){
+    return(
+        <div className='container-card'>
+            <div className="container-info">
+                <h1>
+                Get <span>insights</span> that help <br /> your busines grow.
+                </h1>
+                <p className='area-txt'>
+                Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall eficciency.
+                </p>
+                <div className="container-num">
+                    <div className="num1"><h2>10K+</h2><p className='txt-num'>COMPANTES</p></div>
+                    <div className="num2"><h2>314</h2><p className='txt-num'>TEMPLATES</p></div>
+                    <div className="num3"><h2>12M+</h2><p className='txt-num'>QUERTES</p></div>
+                    
+                </div>
+            </div>
+            <div className='container-img'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Card
---
+++ desfioComponente/src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+import '../../styles/home.sass'
+import Card from '../../components/card';
+
+function Home() {
+  return (
+      <Card />
+  );
+}
+
+export default Home;
